81c2cb8946a38a9daff1e730e7520b267e805c55
Diusrex <killakan002@hotmail.com>
2014-11-26 18:10:57 -0700

    Added tests for PositionTests.

---
+++ src/com/diusrex/tictactoe/tests/PositionTests.java
@@ -0,0 +1,35 @@
+package com.diusrex.tictactoe.tests;
+
+import org.junit.Test;
+
+import com.diusrex.tictactoe.BoxPosition;
+import com.diusrex.tictactoe.SectionPosition;
+
+public class PositionTests {
+    @Test
+    public void testPositionToSection() {
+        BoxPosition boxPos = new BoxPosition(0, 0);
+        TestUtils.assertAreEqual(new SectionPosition(0, 0),
+                boxPos.getSectionIn());
+
+        boxPos = new BoxPosition(3, 8);
+        TestUtils.assertAreEqual(new SectionPosition(1, 2),
+                boxPos.getSectionIn());
+
+        boxPos = new BoxPosition(5, 1);
+        TestUtils.assertAreEqual(new SectionPosition(1, 0),
+                boxPos.getSectionIn());
+    }
+
+    @Test
+    public void testSectionToPosition() {
+        SectionPosition sectionPos = new SectionPosition(0, 0);
+        TestUtils.assertAreEqual(new BoxPosition(0, 0),
+                sectionPos.getTopLeftPosition());
+
+        sectionPos = new SectionPosition(1, 2);
+        TestUtils.assertAreEqual(new BoxPosition(3, 6),
+                sectionPos.getTopLeftPosition());
+    }
+
+}
