20716d52daf33990df9715f3b5007ea61558112e
Diusrex <killakan002@hotmail.com>
2014-12-06 22:33:19 -0700

    Improved getString of Player

--- src/com/diusrex/tictactoe/logic/Player.java
+++ src/com/diusrex/tictactoe/logic/Player.java
@@ -1,5 +1,15 @@
 package com.diusrex.tictactoe.logic;
 
 public enum Player {
-    Unowned, Player_1, Player_2
+    Unowned("0"), Player_1("1"), Player_2("2");
+    
+    String name;
+    Player(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
 }
