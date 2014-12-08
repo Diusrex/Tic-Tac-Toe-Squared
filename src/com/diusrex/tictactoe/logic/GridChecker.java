f0d44336a54540994ba45032ec7a6b79e230760e
Diusrex <killakan002@hotmail.com>
2014-12-07 21:00:30 -0700

    Created GridChecker with its tests.
    It will check for patterns in the given grid.

---
+++ src/com/diusrex/tictactoe/logic/GridChecker.java
@@ -0,0 +1,116 @@
+package com.diusrex.tictactoe.logic;
+
+/*
+ * GridChecker
+ * This class is built around checking to see if a player has made a pattern of 3 inside of the given grid, or optionally inside of the
+ * section within the grid
+ */
+public class GridChecker {
+    static final int LINE_LENGTH = 3;
+
+    public static Player searchForPattern(Player[][] grid) {
+        return searchForPattern(grid, new SectionPosition(0, 0));
+    }
+
+    // Will return the first pattern it finds. If there are multiple, then it
+    // will return the first
+    public static Player searchForPattern(Player[][] grid,
+            SectionPosition sectionIn) {
+        if (!gridIsValid(grid, sectionIn))
+            return Player.Unowned;
+        
+        Player foundPlayer = searchForHorizontalPattern(grid, sectionIn);
+
+        if (foundPlayer != Player.Unowned)
+            return foundPlayer;
+
+        foundPlayer = searchForVerticalPattern(grid, sectionIn);
+
+        if (foundPlayer != Player.Unowned)
+            return foundPlayer;
+
+        return searchForDiagonalPattern(grid, sectionIn);
+    }
+
+    private static boolean gridIsValid(Player[][] grid,
+            SectionPosition sectionIn) {
+        if (grid == null)
+            return false;
+        
+        boolean validXSpot = sectionIsValid(sectionIn.getX(), grid);
+        boolean validYSpot = sectionIsValid(sectionIn.getY(), grid);
+        return validXSpot && validYSpot;
+    }
+
+    private static boolean sectionIsValid(int sectionValue, Player[][] grid) {
+        return sectionValue >= 0 && sectionValue * BoardStatus.SIZE_OF_SECTION < grid.length;
+    }
+
+    private static Player searchForHorizontalPattern(Player[][] grid,
+            SectionPosition sectionIn) {
+        BoxPosition currentPos = sectionIn.getTopLeftPosition();
+        BoxPosition horizontalIncrease = new BoxPosition(1, 0);
+        BoxPosition verticalIncrease = new BoxPosition(0, 1);
+
+        for (int i = 0; i < LINE_LENGTH; ++i, currentPos = currentPos
+                .increaseBy(verticalIncrease)) {
+            if (lineOwnedBySinglePlayer(grid, currentPos, horizontalIncrease))
+                return getPlayer(grid, currentPos);
+        }
+        return Player.Unowned;
+    }
+
+    private static Player searchForVerticalPattern(Player[][] grid,
+            SectionPosition sectionIn) {
+        BoxPosition currentPos = sectionIn.getTopLeftPosition();
+        BoxPosition horizontalIncrease = new BoxPosition(1, 0);
+        BoxPosition verticalIncrease = new BoxPosition(0, 1);
+
+        for (int i = 0; i < LINE_LENGTH; ++i, currentPos = currentPos
+                .increaseBy(horizontalIncrease)) {
+            if (lineOwnedBySinglePlayer(grid, currentPos, verticalIncrease))
+                return getPlayer(grid, currentPos);
+        }
+        return Player.Unowned;
+    }
+
+    private static Player searchForDiagonalPattern(Player[][] grid,
+            SectionPosition sectionIn) {
+        BoxPosition currentPos = sectionIn.getTopLeftPosition();
+        BoxPosition diagonalIncrease = new BoxPosition(1, 1);
+
+        if (lineOwnedBySinglePlayer(grid, currentPos, diagonalIncrease))
+            return getPlayer(grid, currentPos);
+
+        BoxPosition differenceBetweenDiagonals = new BoxPosition(2, 0);
+        currentPos = currentPos.increaseBy(differenceBetweenDiagonals);
+
+        diagonalIncrease = new BoxPosition(-1, 1);
+
+        if (lineOwnedBySinglePlayer(grid, currentPos, diagonalIncrease))
+            return getPlayer(grid, currentPos);
+
+        return Player.Unowned;
+    }
+
+    private static boolean lineOwnedBySinglePlayer(Player[][] grid,
+            BoxPosition lineStart, BoxPosition increase) {
+        final Player firstPlayer = getPlayer(grid, lineStart);
+        if (firstPlayer == Player.Unowned)
+            return false;
+
+        BoxPosition currentSpot = lineStart;
+        // Could skip the spot at 0, but this works too
+        for (int i = 0; i < LINE_LENGTH; ++i, currentSpot = currentSpot
+                .increaseBy(increase)) {
+            if (firstPlayer != getPlayer(grid, currentSpot))
+                return false;
+        }
+
+        return true;
+    }
+
+    private static Player getPlayer(Player[][] grid, BoxPosition pos) {
+        return grid[pos.getX()][pos.getY()];
+    }
+}
---
+++ src/com/diusrex/tictactoe/logic/tests/GridCheckerTests.java
@@ -0,0 +1,168 @@
+package com.diusrex.tictactoe.logic.tests;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.diusrex.tictactoe.logic.BoardStatus;
+import com.diusrex.tictactoe.logic.BoxPosition;
+import com.diusrex.tictactoe.logic.GridChecker;
+import com.diusrex.tictactoe.logic.Player;
+import com.diusrex.tictactoe.logic.SectionPosition;
+
+public class GridCheckerTests {
+    static final int SIZE = 9;
+    Player[][] grid;
+
+    static final BoxPosition horizontalIncrease = new BoxPosition(1, 0);
+    static final BoxPosition verticalIncrease = new BoxPosition(0, 1);
+
+    @Before
+    public void setUp() {
+        grid = new Player[SIZE][SIZE];
+
+        resetGrid();
+    }
+
+    private void resetGrid() {
+        for (int y = 0; y < grid.length; ++y) {
+            for (int x = 0; x < grid.length; ++x) {
+                grid[x][y] = Player.Unowned;
+            }
+        }
+    }
+
+    @Test
+    public void testNoPattern() {
+        Assert.assertEquals(Player.Unowned,
+                GridChecker.searchForPattern(grid, new SectionPosition(0, 0)));
+    }
+
+    @Test
+    public void testNullGridGiven() {
+        GridChecker.searchForPattern(null, new SectionPosition(0, 0));
+    }
+
+    // These are all invalid positions for the given grid
+    @Test
+    public void testSectionOutside() {
+        final int NUMBER_OF_SECTIONS_IN_GRID = 1;
+        grid = new Player[NUMBER_OF_SECTIONS_IN_GRID * BoardStatus.SIZE_OF_SECTION]
+                [NUMBER_OF_SECTIONS_IN_GRID * BoardStatus.SIZE_OF_SECTION];
+
+        SectionPosition sectionIn = new SectionPosition(-1, 0);
+        GridChecker.searchForPattern(grid, sectionIn);
+
+        sectionIn = new SectionPosition(NUMBER_OF_SECTIONS_IN_GRID, 0);
+        GridChecker.searchForPattern(grid, sectionIn);
+
+        sectionIn = new SectionPosition(0, -1);
+        GridChecker.searchForPattern(grid, sectionIn);
+
+        sectionIn = new SectionPosition(0, NUMBER_OF_SECTIONS_IN_GRID);
+        GridChecker.searchForPattern(grid, sectionIn);
+    }
+
+    @Test
+    public void testHorizontalPatterns() {
+        BoxPosition startPos = new BoxPosition(0, 0);
+        BoxPosition finalPos = new BoxPosition(2, 0);
+        SectionPosition sectionIn = startPos.getSectionIn();
+
+        Player currentPlayer = Player.Player_1;
+
+        for (int i = 0; i < 3; ++i, startPos = startPos
+                .increaseBy(verticalIncrease), finalPos = finalPos
+                .increaseBy(verticalIncrease)) {
+            // Will not completely fill the line
+            fillLine(startPos, horizontalIncrease, currentPlayer, 2);
+            Assert.assertEquals(Player.Unowned,
+                    GridChecker.searchForPattern(grid, sectionIn));
+
+            setGridPlayer(finalPos, currentPlayer);
+
+            Assert.assertEquals(currentPlayer,
+                    GridChecker.searchForPattern(grid, sectionIn));
+            resetGrid();
+        }
+    }
+
+    @Test
+    public void testVerticalPatterns() {
+        BoxPosition startPos = new BoxPosition(3, 6);
+        BoxPosition finalPos = new BoxPosition(3, 8);
+        SectionPosition sectionIn = startPos.getSectionIn();
+
+        Player currentPlayer = Player.Player_1;
+
+        for (int i = 0; i < 3; ++i, startPos = startPos
+                .increaseBy(horizontalIncrease), finalPos = finalPos
+                .increaseBy(horizontalIncrease)) {
+            // Will not completely fill the line
+            fillLine(startPos, verticalIncrease, currentPlayer, 2);
+            Assert.assertEquals(Player.Unowned,
+                    GridChecker.searchForPattern(grid, sectionIn));
+
+            setGridPlayer(finalPos, currentPlayer);
+
+            Assert.assertEquals(currentPlayer,
+                    GridChecker.searchForPattern(grid, sectionIn));
+            resetGrid();
+        }
+    }
+
+    @Test
+    public void testDiagonalPatternOne() {
+        BoxPosition startPos = new BoxPosition(3, 3);
+        BoxPosition finalPos = new BoxPosition(5, 5);
+        SectionPosition sectionIn = startPos.getSectionIn();
+
+        Player currentPlayer = Player.Player_2;
+
+        BoxPosition diagonalIncrease = new BoxPosition(1, 1);
+
+        fillLine(startPos, diagonalIncrease, currentPlayer, 2);
+
+        Assert.assertEquals(Player.Unowned,
+                GridChecker.searchForPattern(grid, sectionIn));
+
+        setGridPlayer(finalPos, currentPlayer);
+
+        Assert.assertEquals(currentPlayer,
+                GridChecker.searchForPattern(grid, sectionIn));
+    }
+
+    @Test
+    public void testDiagonalPatternTwo() {
+        BoxPosition startPos = new BoxPosition(3, 5);
+        BoxPosition finalPos = new BoxPosition(5, 3);
+        SectionPosition sectionIn = startPos.getSectionIn();
+
+        Player currentPlayer = Player.Player_2;
+
+        BoxPosition diagonalIncrease = new BoxPosition(1, -1);
+
+        fillLine(startPos, diagonalIncrease, currentPlayer, 2);
+
+        Assert.assertEquals(Player.Unowned,
+                GridChecker.searchForPattern(grid, sectionIn));
+
+        setGridPlayer(finalPos, currentPlayer);
+
+        Assert.assertEquals(currentPlayer,
+                GridChecker.searchForPattern(grid, sectionIn));
+    }
+
+    private void fillLine(BoxPosition startPos, BoxPosition increase,
+            Player player, int length) {
+        for (int i = 0; i < length; ++i, startPos = startPos
+                .increaseBy(increase)) {
+            setGridPlayer(startPos, player);
+        }
+    }
+
+    private void setGridPlayer(BoxPosition pos, Player player) {
+        grid[pos.getX()][pos.getY()] = player;
+    }
+}
