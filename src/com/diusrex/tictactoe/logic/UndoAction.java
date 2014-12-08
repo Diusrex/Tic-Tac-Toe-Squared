2ee2b98ca6b5a56ffad847cfb7538564fc64ae15
Diusrex <killakan002@hotmail.com>
2014-12-07 21:33:39 -0700

    Set up UndoAction and its tests.

---
+++ src/com/diusrex/tictactoe/logic/UndoAction.java
@@ -0,0 +1,56 @@
+package com.diusrex.tictactoe.logic;
+
+import java.util.Stack;
+
+public class UndoAction {
+    public static void undoLastMove(BoardStatus board) {
+        Stack<Move> allMoves = board.getAllMoves();
+        // Cannot do anything in this case
+        if (allMoves.size() == 0)
+            return;
+
+        // Take out the top move
+        Move topMove = allMoves.pop();
+
+        restoreBoxToUnowned(board, topMove.getPosition());
+
+        if (moveLostOwnership(board, topMove))
+            board.setSectionOwner(topMove.getSectionIn(), Player.Unowned);
+
+        restoreSectionToPlayIn(board, allMoves, topMove.getPosition());
+    }
+
+    private static void restoreBoxToUnowned(BoardStatus board, BoxPosition pos) {
+        board.setBoxOwner(pos, Player.Unowned);
+    }
+
+    private static boolean moveLostOwnership(BoardStatus board,
+            Move topMove) {
+        // In this case, it is impossible
+        if (topMove.getPlayer() != board.getSectionOwner(topMove.getSectionIn()))
+            return false;
+        
+        SectionPosition sectionIn = topMove.getSectionIn();
+        Player completedWinner = GridChecker.searchForPattern(board.getBoxGrid(), sectionIn);
+        
+        // If there is no found match, then the grid was lost
+        return completedWinner == Player.Unowned;
+    }
+
+    private static void restoreSectionToPlayIn(BoardStatus board,
+            Stack<Move> allMoves, BoxPosition topMove) {
+        SectionPosition sectionToRestoreTo;
+
+        // If > 0, can get where to play from previous move
+        if (allMoves.size() > 0) {
+            Move previousMove = allMoves.peek();
+
+            sectionToRestoreTo = TicTacToeEngine
+                    .getSectionToPlayInNext(previousMove);
+        } else {
+            sectionToRestoreTo = topMove.getSectionIn();
+        }
+
+        board.setSectionToPlayIn(sectionToRestoreTo);
+    }
+}
---
+++ src/com/diusrex/tictactoe/logic/tests/UndoActionTests.java
@@ -0,0 +1,180 @@
+package com.diusrex.tictactoe.logic.tests;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.diusrex.tictactoe.logic.BoardStatus;
+import com.diusrex.tictactoe.logic.BoxPosition;
+import com.diusrex.tictactoe.logic.Move;
+import com.diusrex.tictactoe.logic.Player;
+import com.diusrex.tictactoe.logic.SectionPosition;
+import com.diusrex.tictactoe.logic.TicTacToeEngine;
+import com.diusrex.tictactoe.logic.UndoAction;
+import com.diusrex.tictactoe.logic.tests.TestUtils.BoardStatusNoCount;
+
+public class UndoActionTests {
+    BoardStatusNoCount board;
+
+    Player mainPlayer;
+
+    // Backup state
+    int stackSize;
+    Player expectedPlayer;
+    SectionPosition origionalSectionToPlayIn;
+    Player[][] origionalBoxOwners;
+    Player[][] origionalSectionOwners;
+
+    SectionPosition mainSection;
+    Move appliedMove;
+    Move validMove;
+
+    @Before
+    public void setup() {
+        board = new BoardStatusNoCount(new SectionPosition(0, 0));
+        mainPlayer = Player.Player_1;
+        board.playerToGoNext = mainPlayer;
+
+        mainSection = new SectionPosition(1, 1);
+        appliedMove = new Move(new BoxPosition(1, 1), mainPlayer);
+
+        TicTacToeEngine.applyMove(board, appliedMove);
+
+        validMove = new Move(new BoxPosition(3, 3), mainPlayer);
+
+        backupBoardState();
+    }
+
+    @Test
+    public void testUndoWithNoMovesLeft() {
+        // This will be fine
+        UndoAction.undoLastMove(board);
+
+        // This should not throw
+        try {
+            UndoAction.undoLastMove(board);
+        } catch (Exception e) {
+            Assert.assertTrue(false);
+        }
+    }
+
+    @Test
+    public void testUndoMove() {
+        TestUtils.applyMoveToBoard(board, validMove);
+
+        UndoAction.undoLastMove(board);
+
+        assertBoardStateUnchanged();
+    }
+
+    @Test
+    public void testUndoMoveFromFull() {
+        SectionPosition fullSection = new SectionPosition(1, 1);
+        fillSection(fullSection);
+
+        BoxPosition pos = new BoxPosition(8, 8);
+        Move move = new Move(pos, mainPlayer);
+
+        board.setSectionToPlayIn(fullSection);
+        backupBoardState();
+        
+        TestUtils.applyMoveToBoard(board, move);
+
+        UndoAction.undoLastMove(board);
+
+        assertBoardStateUnchanged();
+    }
+
+    private void fillSection(SectionPosition fullSection) {
+        BoxPosition offset = fullSection.getTopLeftPosition();
+        for (int x = 0; x < 3; ++x) {
+            for (int y = 0; y < 3; ++y) {
+                BoxPosition basePosition = new BoxPosition(x, y);
+                board.setBoxOwner(basePosition.increaseBy(offset), mainPlayer);
+            }
+        }
+    }
+
+    @Test
+    public void testUndoSectionWinningMove() {
+        SectionPosition sectionToWin = new SectionPosition(0, 0);
+        winSection(sectionToWin);
+        
+        Assert.assertEquals(mainPlayer, board.getSectionOwner(sectionToWin));
+        UndoAction.undoLastMove(board);
+
+        Assert.assertEquals(Player.Unowned, board.getSectionOwner(sectionToWin));
+    }
+    
+    @Test
+    public void testUndoSectionDoesntLoseSection() {
+        SectionPosition sectionToWin = new SectionPosition(0, 0);
+        winSection(sectionToWin);
+        
+        Assert.assertEquals(mainPlayer, board.getSectionOwner(sectionToWin));
+        
+        board.setSectionToPlayIn(sectionToWin);
+        BoxPosition moveThatDoesntEffectOwnership = new BoxPosition(2, 2);
+        TicTacToeEngine.applyMove(board, new Move(moveThatDoesntEffectOwnership, mainPlayer));
+        
+        UndoAction.undoLastMove(board);
+        
+        Assert.assertEquals(mainPlayer, board.getSectionOwner(sectionToWin));
+    }
+    
+    private void winSection(SectionPosition section) {
+        BoxPosition current = section.getTopLeftPosition();
+        BoxPosition increase = new BoxPosition(1, 0);
+        board.setSectionToPlayIn(section);
+        TicTacToeEngine.applyMove(board, new Move(current, mainPlayer));
+        current = current.increaseBy(increase);
+        board.setSectionToPlayIn(section);
+        TicTacToeEngine.applyMove(board, new Move(current, mainPlayer));
+        current = current.increaseBy(increase);
+        board.setSectionToPlayIn(section);
+        TicTacToeEngine.applyMove(board, new Move(current, mainPlayer));
+        current = current.increaseBy(increase);
+    }
+
+    private void backupBoardState() {
+        stackSize = board.getAllMoves().size();
+        origionalSectionToPlayIn = board.getSectionToPlayIn();
+
+        backupBoxOwners();
+
+        backupSectionOwners();
+    }
+
+    private void backupBoxOwners() {
+        origionalBoxOwners = new Player[BoardStatus.NUMBER_OF_BOXES_PER_SIDE][BoardStatus.NUMBER_OF_BOXES_PER_SIDE];
+        for (int x = 0; x < BoardStatus.NUMBER_OF_BOXES_PER_SIDE; ++x)
+            for (int y = 0; y < BoardStatus.NUMBER_OF_BOXES_PER_SIDE; ++y)
+                origionalBoxOwners[x][y] = board.getBoxOwner(new BoxPosition(x,
+                        y));
+    }
+
+    private void backupSectionOwners() {
+        origionalSectionOwners = new Player[BoardStatus.NUMBER_OF_SECTIONS_PER_SIDE][BoardStatus.NUMBER_OF_SECTIONS_PER_SIDE];
+        for (int x = 0; x < BoardStatus.NUMBER_OF_SECTIONS_PER_SIDE; ++x)
+            for (int y = 0; y < BoardStatus.NUMBER_OF_SECTIONS_PER_SIDE; ++y)
+                origionalSectionOwners[x][y] = board
+                        .getSectionOwner(new SectionPosition(x, y));
+    }
+
+    private void assertBoardStateUnchanged() {
+        Assert.assertEquals(stackSize, board.getAllMoves().size());
+        TestUtils.assertAreEqual(origionalSectionToPlayIn,
+                board.getSectionToPlayIn());
+
+        for (int x = 0; x < BoardStatus.NUMBER_OF_BOXES_PER_SIDE; ++x)
+            for (int y = 0; y < BoardStatus.NUMBER_OF_BOXES_PER_SIDE; ++y)
+                Assert.assertEquals(origionalBoxOwners[x][y],
+                        board.getBoxOwner(new BoxPosition(x, y)));
+
+        for (int x = 0; x < BoardStatus.NUMBER_OF_SECTIONS_PER_SIDE; ++x)
+            for (int y = 0; y < BoardStatus.NUMBER_OF_SECTIONS_PER_SIDE; ++y)
+                Assert.assertEquals(origionalSectionOwners[x][y],
+                        board.getSectionOwner(new SectionPosition(x, y)));
+    }
+}
