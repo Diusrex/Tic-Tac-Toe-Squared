bc39d485bac993fbe77cf6f49cac95841be607e6
Diusrex <killakan002@hotmail.com>
2014-12-16 23:48:53 -0700

    Created MyGrid.
    It is a GridLayout that is able to draw a single line between the center of two views in it.

---
+++ app/src/main/java/com/diusrex/tictactoe/MyGrid.java
@@ -0,0 +1,71 @@
+package com.diusrex.tictactoe;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.GridLayout;
+
+public class MyGrid extends GridLayout {
+    View lineStart;
+    View lineEnd;
+    Paint paint;
+
+    public MyGrid(Context context) {
+        super(context);
+        paint = new Paint();
+    }
+
+    public MyGrid(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        paint = new Paint();
+    }
+
+    public MyGrid(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public boolean hasLine() {
+        return lineStart != null && lineEnd != null;
+    }
+
+    public void setLine(View lineStart, View lineEnd) {
+        this.lineStart = lineStart;
+        this.lineEnd = lineEnd;
+        invalidate();
+    }
+
+    public void removeLine() {
+        this.lineStart = null;
+        this.lineEnd = null;
+    }
+
+    public void setLineWidth(int width) {
+        paint.setStrokeWidth(width);
+    }
+
+    public void setLineColor(int color) {
+        paint.setColor(color);
+    }
+
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        super.dispatchDraw(canvas);
+
+        drawLine(canvas);
+    }
+
+    private void drawLine(Canvas canvas) {
+
+        if (hasLine()) {
+            int offset = lineStart.getHeight() / 2;
+            canvas.drawLine(lineStart.getX() + offset, lineStart.getY() + offset, lineEnd.getX() + offset, lineEnd.getY() + offset, paint);
+
+            // Have the line be rounded
+            canvas.drawCircle(lineStart.getX() + offset, lineStart.getY() + offset, paint.getStrokeWidth() / 2, paint);
+            canvas.drawCircle(lineEnd.getX() + offset, lineEnd.getY() + offset, paint.getStrokeWidth() / 2, paint);
+        }
+    }
+}
