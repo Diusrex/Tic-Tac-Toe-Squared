6eb28a4e63af78e5b61b170c7317bc1959149706
Diusrex <killakan002@hotmail.com>
2014-12-27 14:36:56 -0700

    Created DrawDialogFragment using gameEndDialogFragment.

---
+++ app/src/main/java/com/diusrex/tictactoe/android/dialogs/DrawDialogFragment.java
@@ -0,0 +1,29 @@
+package com.diusrex.tictactoe.android.dialogs;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+
+import com.diusrex.tictactoe.R;
+
+// Must be attached to an activity that extends the WinDialogListener
+public class DrawDialogFragment extends GameEndDialogFragment {
+    public static DrawDialogFragment newInstance() {
+        return new DrawDialogFragment();
+    }
+
+    @Override
+    protected void loadArguments(Bundle arguments) {
+        // No need to load anything
+    }
+
+    @Override
+    protected AlertDialog.Builder setUpOutput(AlertDialog.Builder builder) {
+        builder.setTitle(R.string.draw_dialog_title);
+
+        String message = getString(R.string.draw_output);
+        builder.setMessage(message);
+
+        return builder;
+    }
+}
+
