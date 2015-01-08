d0bc7bd8bc02275aee8c8039729311d307cbcce3
Diusrex <killakan002@hotmail.com>
2015-01-08 14:05:27 -0700

    Set up PossibleToWinChecker with tests.

---
+++ app/src/main/java/com/diusrex/tictactoe/logic/PossibleToWinChecker.java
@@ -0,0 +1,72 @@
+package com.diusrex.tictactoe.logic;
+
+public class PossibleToWinChecker {
+    static private final SectionPosition horizontalIncrease = SectionPosition.make(1, 0);
+    static private final SectionPosition verticalIncrease = SectionPosition.make(0, 1);
+    static private final SectionPosition topLeftToBottomRightIncrease = SectionPosition.make(1, 1);
+    static private final SectionPosition topRightToBottomLeftIncrease = SectionPosition.make(-1, 1);
+
+    public static boolean isStillPossibleToWin(BoardStatus board) {
+        if (TicTacToeEngine.getWinner(board) != Player.Unowned)
+            return false;
+
+        // Will search through each line, and check to see if it does not
+        // contain different players
+        if (canWinHorizontal(board))
+            return true;
+        if (canWinVertical(board))
+            return true;
+        if (canWinDiagonalTL_To_BR(board))
+            return true;
+        if (canWinDiagonalTR_To_BL(board))
+            return true;
+        return false;
+    }
+
+    private static boolean canWinHorizontal(BoardStatus board) {
+        SectionPosition start = SectionPosition.make(0, 0);
+        for (int i = 0; i < BoardStatus.NUMBER_OF_SECTIONS_PER_SIDE; ++i, start = start.increaseBy(verticalIncrease)) {
+            if (lineContainsOwnedAndOrSinglePlayer(board, start, horizontalIncrease))
+                return true;
+        }
+        return false;
+    }
+
+    private static boolean canWinVertical(BoardStatus board) {
+        SectionPosition start = SectionPosition.make(0, 0);
+        for (int i = 0; i < BoardStatus.NUMBER_OF_SECTIONS_PER_SIDE; ++i, start = start.increaseBy(horizontalIncrease)) {
+            if (lineContainsOwnedAndOrSinglePlayer(board, start, verticalIncrease))
+                return true;
+        }
+        return false;
+    }
+
+    private static boolean canWinDiagonalTL_To_BR(BoardStatus board) {
+        return lineContainsOwnedAndOrSinglePlayer(board, SectionPosition.make(0, 0), topLeftToBottomRightIncrease);
+    }
+
+    private static boolean canWinDiagonalTR_To_BL(BoardStatus board) {
+        return lineContainsOwnedAndOrSinglePlayer(board, SectionPosition.make(2, 0), topRightToBottomLeftIncrease);
+        }
+
+    private static boolean lineContainsOwnedAndOrSinglePlayer(BoardStatus board, SectionPosition start,
+            SectionPosition increase) {
+        boolean containsPlayerOne = false;
+        boolean containsPlayerTwo = false;
+        boolean empty = true;
+
+        for (int i = 0; i < BoardStatus.NUMBER_OF_SECTIONS_PER_SIDE; ++i, start = start.increaseBy(increase)) {
+            Player p = board.getSectionOwner(start);
+            if (p != Player.Unowned) {
+                empty = false;
+
+                if (p == Player.Player_1)
+                    containsPlayerOne = true;
+                else
+                    containsPlayerTwo = true;
+            }
+        }
+
+        return containsPlayerOne ^ containsPlayerTwo ^ empty;
+    }
+}
---
+++ tests/com/diusrex/tictactoe/logic/tests/PossibleToWinCheckerTests.java
@@ -0,0 +1,116 @@
+package com.diusrex.tictactoe.logic.tests;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.diusrex.tictactoe.logic.BoardStatus;
+import com.diusrex.tictactoe.logic.Player;
+import com.diusrex.tictactoe.logic.PossibleToWinChecker;
+import com.diusrex.tictactoe.logic.SectionPosition;
+
+public class PossibleToWinCheckerTests {
+    BoardStatus board;
+
+    @Before
+    public void setup() {
+        board = new BoardStatus(SectionPosition.make(0, 0));
+    }
+
+    @Test
+    public void testBoardCanBeWonSimple() {
+        Assert.assertTrue(PossibleToWinChecker.isStillPossibleToWin(board));
+    }
+
+    // 1 1 2
+    // 2 2 1
+    // 1 0 0
+    @Test
+    public void testBoardCanBeWonComplexHorizontalOnly() {
+        takeOwnershipOfSection(SectionPosition.make(0, 0), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(1, 0), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(2, 0), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(0, 1), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(1, 1), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(2, 1), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(0, 2), Player.Player_1);
+        Assert.assertTrue(PossibleToWinChecker.isStillPossibleToWin(board));
+    }
+
+    // 1 2 1
+    // 0 2 1
+    // 0 1 2
+    @Test
+    public void testBoardCanBeWonComplexVerticalOnly() {
+        takeOwnershipOfSection(SectionPosition.make(0, 0), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(1, 0), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(2, 0), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(1, 1), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(2, 1), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(1, 2), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(2, 2), Player.Player_2);
+        Assert.assertTrue(PossibleToWinChecker.isStillPossibleToWin(board));
+    }
+    
+    // 1 2 1
+    // 1 0 2
+    // 2 1 1
+    @Test
+    public void testBoardCanBeWonComplexDiagonalTL_To_BR_Only() {
+        takeOwnershipOfSection(SectionPosition.make(0, 0), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(1, 0), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(2, 0), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(0, 1), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(2, 1), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(0, 2), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(1, 2), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(2, 2), Player.Player_1);
+        Assert.assertTrue(PossibleToWinChecker.isStillPossibleToWin(board));
+    }
+    
+    // 2 1 2
+    // 1 0 2
+    // 2 2 1
+    @Test
+    public void testBoardCanBeWonComplexDiagonalTR_To_BL_Only() {
+        takeOwnershipOfSection(SectionPosition.make(0, 0), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(1, 0), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(2, 0), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(0, 1), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(2, 1), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(0, 2), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(1, 2), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(2, 2), Player.Player_1);
+        Assert.assertTrue(PossibleToWinChecker.isStillPossibleToWin(board));
+    }
+
+    @Test
+    public void testBoardCannotBeWonWhenIsWon() {
+        takeOwnershipOfSection(SectionPosition.make(0, 0), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(1, 0), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(2, 0), Player.Player_1);
+        Assert.assertFalse(PossibleToWinChecker.isStillPossibleToWin(board));
+    }
+
+    // 1 2 1
+    // 1 2 1
+    // 2 1 2
+    @Test
+    public void testBoardCannotBeWonBoardIsFull() {
+        takeOwnershipOfSection(SectionPosition.make(0, 0), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(1, 0), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(2, 0), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(0, 1), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(1, 1), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(2, 1), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(0, 2), Player.Player_2);
+        takeOwnershipOfSection(SectionPosition.make(1, 2), Player.Player_1);
+        takeOwnershipOfSection(SectionPosition.make(2, 2), Player.Player_2);
+        Assert.assertFalse(PossibleToWinChecker.isStillPossibleToWin(board));
+    }
+
+    private void takeOwnershipOfSection(SectionPosition section, Player player) {
+        board.setSectionOwner(section, null, player);
+    }
+}
