fcaadab5c2fcc1a3149927fbc419c84d7467f47b
Diusrex <killakan002@hotmail.com>
2015-01-25 21:27:26 -0700

    Minor touchups.

--- app/src/main/java/com/diusrex/tictactoe/android/ActivityMainMenu.java
+++ app/src/main/java/com/diusrex/tictactoe/android/ActivityMainMenu.java
@@ -13,7 +13,6 @@ public class ActivityMainMenu extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-
     }
 
     @Override
