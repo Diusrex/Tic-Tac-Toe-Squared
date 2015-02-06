2ab1f51cc2874e20da3dbdaa61920a7eccbf9df2
Diusrex <killakan002@hotmail.com>
2015-02-06 12:31:59 -0700

    Added Apache 2.0 license.

--- app/src/main/java/com/diusrex/tictactoe/textbased/Main.java
+++ app/src/main/java/com/diusrex/tictactoe/textbased/Main.java
@@ -1,12 +1,27 @@
+/*
+ * Copyright 2015 Morgan Redshaw
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
 package com.diusrex.tictactoe.textbased;
-
-import java.util.Scanner;
 
 import com.diusrex.tictactoe.logic.BoardStatus;
 import com.diusrex.tictactoe.logic.BoxPosition;
 import com.diusrex.tictactoe.logic.Move;
 import com.diusrex.tictactoe.logic.Player;
 import com.diusrex.tictactoe.logic.TicTacToeEngine;
+
+import java.util.Scanner;
 
 public class Main {
     static PlayerController[] players = new PlayerController[2];
