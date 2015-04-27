d9f1a0f3a5287c57158b86ef679db0605bb4f615
Diusrex <killakan002@hotmail.com>
2015-04-27 15:34:30 -0600

    Renamed PlayerScoreCalculator

--- app/src/main/java/com/diusrex/tictactoe/ai/PlayerScoreCalculator.java
+++ app/src/main/java/com/diusrex/tictactoe/ai/PlayerScoreCalculator.java
@@ -3,6 +3,6 @@ package com.diusrex.tictactoe.ai;
 import com.diusrex.tictactoe.data_structures.Grid;
 import com.diusrex.tictactoe.data_structures.Player;
 
-public interface ScoreCalculator {
+public interface PlayerScoreCalculator {
     public int calculateSetupScore(Player currentPlayer, Grid grid, ScoringFunction scoringFunction);
 }
