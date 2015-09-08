1bf32077661cab00a685255f1027efe986fdd260
Diusrex <diusrex@gmail.com>
2015-09-08 15:35:45 -0600

    Set up the FullMonteCarloPlayer and its nodes.
    Still needs quite a bit of work, but can beat some basic players (although is far slower)

---
+++ app/src/main/java/com/diusrex/tictactoe/ai/FullMonteCarloPlayer.java
@@ -0,0 +1,156 @@
+package com.diusrex.tictactoe.ai;
+
+import com.diusrex.tictactoe.data_structures.*;
+import com.diusrex.tictactoe.logic.GridLists;
+import com.diusrex.tictactoe.logic.StringSaver;
+
+import java.util.*;
+
+public class FullMonteCarloPlayer extends AIPlayer {
+
+    public static final String IDENTIFIER = "FullMonteCarlo";
+    private int numberOfIterations;
+
+    private Map<String, FullMonteCarloPlayerStateNode> stateToMove;
+
+    public static FullMonteCarloPlayer getStandardPlayer() {
+        return new FullMonteCarloPlayer(100000);
+    }
+
+    public FullMonteCarloPlayer(int numberOfIterations) {
+        super();
+        stateToMove = new HashMap<>();
+        this.numberOfIterations = numberOfIterations;
+    }
+
+    @Override
+    protected Move choosePosition(BoardStatus board) {
+
+        // The starting node is created from the move belonging to the OTHER player
+        FullMonteCarloPlayerStateNode startingNode = stateToMove.get(StringSaver.getSaveString(board));
+        if (startingNode == null) {
+            startingNode = new FullMonteCarloPlayerStateNode(board.getNextPlayer().opposite(), null);
+        }
+
+        if (startingNode.isLeaf()) {
+            startingNode.expand(getAllValidMoves(board));
+        }
+
+        Player currentPlayer = board.getNextPlayer();
+        Random random = getRandom();
+
+        for (int i = 0; i < numberOfIterations; ++i) {
+            runOneIteration(board, startingNode, currentPlayer, random);
+        }
+
+        return startingNode.getBestMove();
+    }
+
+    protected Random getRandom() {
+        return new Random();
+    }
+
+    private void runOneIteration(BoardStatus board, FullMonteCarloPlayerStateNode startingNode, Player mainPlayer, Random random) {
+        List<FullMonteCarloPlayerStateNode> visited = new LinkedList<>();
+        FullMonteCarloPlayerStateNode currentNode = startingNode.select(random); // Don't want to apply the startingNode, or even update its stats
+
+        // Need to handle the possibility that it has reached a draw, or no valid moves exist
+        while (currentNode != null && !currentNode.isLeaf()) {
+            visited.add(currentNode);
+            board.applyMoveIfValid(currentNode.getMove());
+
+            currentNode = currentNode.select(random);
+        }
+
+        Player winner;
+        if (currentNode != null) {
+            // Need to add currentNode to the list, and apply it to the board
+            visited.add(currentNode);
+            board.applyMoveIfValid(currentNode.getMove());
+
+            currentNode.expand(getAllValidMoves(board));
+
+            // Save the current node into the tree
+            if (board.getNextPlayer() == mainPlayer) { // But no need to save the player who is not going next
+                stateToMove.put(StringSaver.getSaveString(board), currentNode);
+            }
+
+            winner = winsRandomGame(board, random);
+        } else {
+            winner = Player.Unowned; // In this case, the node encountered was a draw
+        }
+
+        updateStats(board, visited, winner);
+
+        // There is no need to update the startingNode, because it was actually the opponents move
+    }
+
+    private void updateStats(BoardStatus board, List<FullMonteCarloPlayerStateNode> visited, Player winner) {
+        for (FullMonteCarloPlayerStateNode nodeVisited : visited) {
+            // Reset the board state, and update all values
+            nodeVisited.updateStats(winner);
+            //System.out.println("Undoing for " + nodeVisited);
+            board.undoLastMove();
+        }
+    }
+
+    // TODO: If this is fairly efficient, could apply to all of the AI's
+    private List<Move> getAllValidMoves(BoardStatus board) {
+        if (canPlayInAnySection(board)) {
+            return getValidMovesInAllSections(board);
+        } else {
+            return getValidMovesInRequiredSection(board);
+        }
+    }
+
+    private List<Move> getValidMovesInAllSections(BoardStatus board) {
+        List<Move> allValidMoves = new ArrayList<>();
+
+        for (SectionPosition section : GridLists.getAllStandardSections()) {
+            addValidMovesInSection(board, section, allValidMoves);
+        }
+
+        return allValidMoves;
+    }
+
+    private List<Move> getValidMovesInRequiredSection(BoardStatus board) {
+        List<Move> allValidMoves = new ArrayList<>();
+        addValidMovesInSection(board, board.getSectionToPlayIn(), allValidMoves);
+
+        return allValidMoves;
+    }
+
+    private void addValidMovesInSection(BoardStatus board, SectionPosition section, List<Move> allValidMoves) {
+        for (BoxPosition pos : GridLists.getAllStandardBoxPositions()) {
+            Move move = Move.make(section, pos, board.getNextPlayer());
+
+            if (board.isValidMove(move)) {
+                allValidMoves.add(move);
+            }
+        }
+    }
+
+    private Player winsRandomGame(BoardStatus board, Random random) {
+        Player winner = board.getWinner();
+        if (winner == Player.Unowned) {
+            List<Move> allValidMoves = getAllValidMoves(board);
+            if (allValidMoves.size() == 0) {
+                return Player.Unowned;
+            }
+
+            int chosen = random.nextInt(allValidMoves.size());
+            board.applyMoveIfValid(allValidMoves.get(chosen));
+
+            winner = winsRandomGame(board, random);
+
+            board.undoLastMove();
+        }
+
+        return winner;
+    }
+
+    @Override
+    public String getIdentifier() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/diusrex/tictactoe/ai/FullMonteCarloPlayerStateNode.java
@@ -0,0 +1,94 @@
+package com.diusrex.tictactoe.ai;
+
+import com.diusrex.tictactoe.data_structures.Move;
+import com.diusrex.tictactoe.data_structures.Player;
+
+import java.util.List;
+import java.util.Random;
+
+/*
+ *  Currently, am trying to reduce the amount of memory used by this.
+ *  Will need to determine the number of nodes that will be created by the end of the game
+ *  Handles storing information about the board status
+ *  Much of the code is from http://mcts.ai/code/java.html
+ *  TODO: Some of this class MUST be synchronized
+ */
+public class FullMonteCarloPlayerStateNode {
+    private Player playerAtNode;
+    private Move move;
+    private FullMonteCarloPlayerStateNode[] allSubnodes = null;
+    private static final double EPSILON = 1e-6;
+    private double numVisits, numWins, winPercent;
+
+    FullMonteCarloPlayerStateNode(Player playerAtNode, Move move) {
+        this.playerAtNode = playerAtNode;
+        this.move = move;
+        numVisits = 0;
+        numWins = 0;
+        winPercent = 0;
+    }
+
+    public Move getMove() {
+        return move;
+    }
+
+    public void expand(List<Move> allMoves) {
+        allSubnodes = new FullMonteCarloPlayerStateNode[allMoves.size()];
+        for (int i = 0; i < allMoves.size(); i++) {
+            Move currentMove = allMoves.get(i);
+            allSubnodes[i] = new FullMonteCarloPlayerStateNode(currentMove.getPlayer(), currentMove);
+        }
+    }
+
+    public boolean isLeaf() {
+        return allSubnodes == null;
+    }
+
+    public FullMonteCarloPlayerStateNode select(Random random) {
+        if (allSubnodes.length == 1) {
+            return allSubnodes[0];
+        }
+
+        FullMonteCarloPlayerStateNode selected = null;
+        double bestValue = Double.MIN_VALUE;
+
+        for (FullMonteCarloPlayerStateNode c : allSubnodes) {
+            double uctValue = getNodeScore(random, c);
+
+            if (uctValue > bestValue) {
+                selected = c;
+                bestValue = uctValue;
+            }
+        }
+
+        return selected;
+    }
+
+    private double getNodeScore(Random random, FullMonteCarloPlayerStateNode node) {
+        // TODO: better to keep 'Math.log(numVisits +1)' inside of loop, or outside?
+        return node.winPercent + Math.sqrt(Math.log(numVisits + 1) / (node.numVisits + EPSILON)) +
+                random.nextDouble() * EPSILON; // Add small random number to break ties
+    }
+
+    public Move getBestMove() {
+        Move bestMove = null;
+        double bestValue = Double.MIN_VALUE;
+
+        for (FullMonteCarloPlayerStateNode c : allSubnodes) {
+            double percent = c.winPercent;
+            if (percent > bestValue) {
+                bestValue = percent;
+                bestMove = c.move;
+            }
+        }
+
+        return bestMove;
+    }
+
+
+    public void updateStats(Player winner) {
+        ++numVisits;
+        numWins += (winner == playerAtNode) ? 1 : 0;
+        winPercent = numWins / numVisits;
+    }
+}
