5a17c8274a78f44a24a0c8a1b0ce416318dfc67e
Diusrex <diusrex@gmail.com>
2015-12-24 11:37:20 -0700

    Improved BaseAlphaBetaPlayer code

--- app/src/main/java/com/diusrex/tictactoe/ai/BaseAlphaBetaPlayer.java
+++ app/src/main/java/com/diusrex/tictactoe/ai/BaseAlphaBetaPlayer.java
@@ -1,18 +1,13 @@
 package com.diusrex.tictactoe.ai;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.diusrex.tictactoe.ai.AIPlayer;
 import com.diusrex.tictactoe.ai.scoring_calculations.Scorer;
 import com.diusrex.tictactoe.ai.scoring_calculations.ScoringValues;
-import com.diusrex.tictactoe.data_structures.BoardStatus;
-import com.diusrex.tictactoe.data_structures.BoxPosition;
-import com.diusrex.tictactoe.data_structures.Move;
-import com.diusrex.tictactoe.data_structures.Player;
-import com.diusrex.tictactoe.data_structures.SectionPosition;
+import com.diusrex.tictactoe.data_structures.*;
 import com.diusrex.tictactoe.logic.GeneralTicTacToeLogic;
 import com.diusrex.tictactoe.logic.GridLists;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  *  Is actually a negamax alpha beta player, due to it being easier to code.
@@ -33,33 +28,32 @@ public abstract class BaseAlphaBetaPlayer extends AIPlayer {
 
     protected abstract int getMaxDepth(BoardStatus board);
 
-    private MoveScore getBestMoveAndItsScore(BoardStatus board, int depth, int alpha, int beta) {
+    private MoveScore getBestMoveAndItsScore(BoardStatus board, int depthLeft, int alpha, int beta) {
         if (board.getWinner() != Player.Unowned) {
-            // The previous player won, but this scoring is for the current
-            // player
-            // Multiplies by depth to prefer (mostly) winning sooner
+            // The previous player won, but this scoring is for the current player
+            // Multiplies by depth to prefer winning sooner, and also causes it to lose later
             // Adds one to ensure that the score wouldn't be 0 if depth is 0
-            return new MoveScore(null, -1 * WIN_SCORE * (depth + 1));
+            return new MoveScore(null, -1 * WIN_SCORE * (depthLeft + 1));
         } else if (GeneralTicTacToeLogic.boardIsFull(board)) {
             return new MoveScore(null, scorer.getTieScore());
-        } else if (depth == 0) {
+        } else if (depthLeft == 0) {
             return new MoveScore(null, scorer.calculateScore(board.getNextPlayer(), board));
         }
-        
+
         List<Move> allMoves = generateMoves(board);
 
         MoveScore bestMove = null;
-        
+
         for (Move move : allMoves) {
             board.applyMoveIfValid(move);
-            int score = - getBestMoveAndItsScore(board, depth - 1, -beta, -alpha).score;
+            int score = - getBestMoveAndItsScore(board, depthLeft - 1, -beta, -alpha).score;
             board.undoLastMove();
-            
+
             if (bestMove == null || score > bestMove.score)
                 bestMove = new MoveScore(move, score);
-            
+
             alpha = Math.max(alpha, score);
-            
+
             // bestMove will not be null, so it is fine
             if (alpha >= beta) {
                 break;
